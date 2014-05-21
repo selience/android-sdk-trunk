f691f346da0258ce3149ec4436f7007bc0f70de1
jacky.lee <selienceblog@sina.com>
2014-05-21 14:56:34 +0800

    modify app icon name

--- gen/com/iresearch/cn/android/R.java
+++ gen/com/iresearch/cn/android/R.java
@@ -979,7 +979,7 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int drawer_shadow=0x7f020057;
         public static final int ic_drawer=0x7f020058;
-        public static final int ic_launcher=0x7f020059;
+        public static final int icon=0x7f020059;
         public static final int nav_main_tab_bg=0x7f02005a;
         public static final int nav_main_tab_divider=0x7f02005b;
         public static final int nav_slide_layout_bg=0x7f02005c;
