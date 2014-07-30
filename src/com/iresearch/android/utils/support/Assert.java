d06d628ef8984899f75f9611e313346f787c42c7
selience <loveselience@gmail.com>
2014-07-30 13:42:04 +0800

    add custom view with pagedvie

---
+++ src/com/iresearch/android/utils/support/Assert.java
@@ -0,0 +1,58 @@
+package com.iresearch.android.utils.support;
+
+public abstract class Assert {
+
+	public static void isTrue(boolean expression, String message) {
+		if (!expression) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isTrue(boolean expression) {
+		isTrue(expression, "[Assertion failed] - this expression must be true");
+	}
+
+	public static void isNull(Object object, String message) {
+		if (object != null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void isNull(Object object) {
+		isNull(object, "[Assertion failed] - the object argument must be null");
+	}
+
+	public static void notNull(Object object, String message) {
+		if (object == null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notNull(Object object) {
+		notNull(object,
+				"[Assertion failed] - this argument is required; it must not be null");
+	}
+
+	public static void hasLength(String text, String message) {
+		if (!(text != null && text.length() > 0)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void hasLength(String text) {
+		hasLength(
+				text,
+				"[Assertion failed] - this String argument must have length; it must not be null or empty");
+	}
+
+	public static void hasText(String text, String message) {
+		if (!(text != null && text.length() > 0)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void hasText(String text) {
+		hasText(text,
+				"[Assertion failed] - this String argument must have text; it must not be null, empty, or blank");
+	}
+}
